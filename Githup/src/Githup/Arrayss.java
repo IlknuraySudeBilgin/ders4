52bab12b0328a8be3916f5e85520a29fbf428028
asus <asus@10.47.176.105>
2022-10-24 11:54:20 +0300

    64

---
+++ Githup/src/Githup/Arrayss.java
@@ -0,0 +1,124 @@
+package Githup;
+
+public class Arrayss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*	int counter;
+		int[] array = new int[10];
+		array[1]=45;
+		System.out.printf("%s%8s%n","Index","Value");
+		
+		for(counter=0;counter<array.length;counter++);
+		{
+			
+			System.out.printf("%5d%8d%n",counter,array[counter]);
+		
+		}*/
+
+		
+		
+		
+		
+		
+		
+		/*int array[] = {32,27,64,18,95,14,90,70,60,37};
+		
+		
+		
+		for(int counter = 0;counter<array.length;counter++)
+		{
+			System.out.printf("%8d" ,array[counter]);
+		}
+		
+		System.out.println();
+		
+		for(int counter=9;counter>=0;counter--)
+		{
+			System.out.printf("%8d"  ,array[counter]);
+		}*/
+		
+		
+		
+		
+		
+		
+		
+	/*	int array[] = {32,27,64,18,95,14,90,60,37};
+		
+		int temp=array[0];
+		array[0]=array[9];
+		array[9]=temp;
+		
+		int counter = 0;
+		System.out.printf("%8d",array[counter]);*/
+		
+		
+		//swap işlemini yap ve github a at
+		
+		
+	/*	for(int i: array)
+	    {
+	    	System.out.println(i+" ");
+	    }
+	    for(int i = 0,j=array.length-1;i<=4;i++,j--)
+	    {
+	      int temp = array[j];
+	      array[j] = array[i];
+	      array[i] = temp;
+	    }
+	    for(int i: array)
+	    {
+	    	System.out.println(i+" ");
+	    	
+	    }*/
+		
+		
+		/*int array[]= {1,2,3,4,5};
+		System.out.printf(
+				"effects of passing referance to entire array:%n" + 
+		"The valuesof the original array are:%n");
+		
+		for(int value : array )
+		{
+			System.out.printf(" %d" ,value);
+		}
+		modifyArray(array);
+		System.out.printf("%n%n the values of the modified array are:%n");
+		
+		for (int value : array)
+		{
+			System.out.printf("%d",value);
+		}
+		
+		System.out.println("%n%n Effects of passing array element value: %n" + 
+		"array[3] before modifyElement : %d%n",array[3]);
+		
+		modifyElement(array[3]);
+		System.out.println(
+				"array[3] after modifyElement : %d%n" , array[3]);
+		
+		
+	}
+
+	private static void modifyElement(int element) {
+		// TODO Auto-generated method stub
+		element *=2;
+		System.out.println(
+				"value of element in modifyElement : %d%n",element());
+		
+	}
+
+	private static void modifyArray(int[] array) {
+		// TODO Auto-generated method stub
+		for(int counter =0;counter<array.length;counter++) {
+			array[counter] *=2;
+		}*/
+		
+		
+		
+		
+	}
+
+}
